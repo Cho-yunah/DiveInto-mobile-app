193721116f3a8615bfbf5ef4eb1285b7da0c7c38
nightohl <haneojin@gmail.com>
2021-06-23 16:20:53 +0900

    feat: 금액 숫자 세자리 단위 컴마 추가

--- src/components/Instructor/MyLecture/Cost.tsx
+++ src/components/Instructor/MyLecture/Cost.tsx
@@ -5,7 +5,7 @@ import { CostStyle as styles } from './styles';
 export default function Cost({ cost }: { cost: number }) {
   return (
     <View style={styles.costContainer}>
-      <Text style={styles.cost}>{cost}</Text>
+      <Text style={styles.cost}>{cost.toLocaleString()}</Text>
       <Text>원</Text>
     </View>
   );
