5ba05a5cef7e9354aaa8f8366c3f654d9d759b83
yongmin kim <kym930722@gmail.com>
2021-07-13 22:09:07 +0900

    fix: eachEquipment 대여장비 개수 조절 버그 수정

--- src/components/ReserveLecture/EachEquipment.tsx
+++ src/components/ReserveLecture/EachEquipment.tsx
@@ -13,6 +13,7 @@ import { useEffect } from 'react';
 import Entype from 'react-native-vector-icons/Entypo';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { useState } from 'react';
+import { useRef } from 'react';
 
 const EachEquipment = ({ item, setIsOpen }: any) => {
   const [displayedPrice, setDisplayedPrice] = useState<number>(item.price);
@@ -82,14 +83,25 @@ const EachEquipController = ({ item, equip }: any) => {
     requestReservationEquipmentState(item.id),
   );
   const studentsNumber = useRecoilValue(studentNumberState);
+
+  let orderNumber: null | number = null;
   const controllStock = (type: 'Increase' | 'Decrease') => {
     // 지금 상태에서 현재 상태의 객체만 제외한 배열을 만들기 위해서 상태값을 담아둔다.
     const currEquips = thisEquipmentArr; // 현재 오리발의 전체 정보를 가진 배열 저장
     const filteredEquips = currEquips.filter(
       // 오리발 전체 배열에서 270값을 수정해야 하므로 그 값만 제외한 배열
-      currEquip =>
-        currEquip.scheduleEquipmentStockId !== equip.scheduleEquipmentStockId,
+      (currEquip, index) => {
+        if (
+          currEquip.scheduleEquipmentStockId !== equip.scheduleEquipmentStockId
+        ) {
+          return true;
+        } else {
+          orderNumber = index;
+          return false;
+        }
+      },
     );
+
     const newEquip = {
       ...equip,
       rentNumber:
@@ -97,7 +109,13 @@ const EachEquipController = ({ item, equip }: any) => {
           ? Math.min(equip.rentNumber + 1, studentsNumber) // 선택한 학생수 이상으로 못 고르게 설정
           : Math.max(equip.rentNumber - 1, 1), // 최소값1
     };
-    setThisEquipmentArr([...filteredEquips, newEquip]); // 정보를 수정한 270사이즈 객체와, 270을 제외한 정보를 가진 배열을 합쳐서 새 상태로 만든다.
+
+    if (orderNumber === null) {
+      setThisEquipmentArr([...filteredEquips, newEquip]); // 정보를 수정한 270사이즈 객체와, 270을 제외한 정보를 가진 배열을 합쳐서 새 상태로 만든다.
+    } else {
+      filteredEquips.splice(orderNumber, 0, newEquip); // 정보를 수정한 270사이즈 객체와, 270을 제외한 정보를 가진 배열을 합쳐서 새 상태로 만든다.
+      setThisEquipmentArr([...filteredEquips]); // 정보를 수정한 270사이즈 객체와, 270을 제외한 정보를 가진 배열을 합쳐서 새 상태로 만든다.
+    }
   };
 
   useEffect(() => {
@@ -113,6 +131,7 @@ const EachEquipController = ({ item, equip }: any) => {
 
     adjustEquipsNumber();
   }, [studentsNumber]);
+  console.log(thisEquipmentArr);
 
   return (
     <View style={styles.sizeItemOuterContainer}>
